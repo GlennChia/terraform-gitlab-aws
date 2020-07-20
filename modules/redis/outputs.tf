95e4ae93368c09121c61b7b1d62a12fc25f8677f
GlennChia <glennchia7@gmail.com>
2020-07-20 21:20:38 +0800

    Renamed output variable to primary_address

--- modules/redis/outputs.tf
+++ modules/redis/outputs.tf
@@ -1,4 +1,4 @@
-output "redis_address" {
+output "primary_address" {
   description = "The address of the endpoint for the primary node in the replication group, if the cluster mode is disabled."
   value       = aws_elasticache_replication_group.this.primary_endpoint_address
 }
