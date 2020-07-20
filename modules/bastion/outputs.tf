6fbe6b98e10992b19a4667bb1b69fe57a273dde9
GlennChia <glennchia7@gmail.com>
2020-07-20 11:34:47 +0800

    Added output for bastion security group

--- modules/bastion/outputs.tf
+++ modules/bastion/outputs.tf
@@ -2,3 +2,8 @@ output "bastion_ip" {
   description = "The elastic ip associated with the Bastion instance"
   value       = aws_eip.this.public_ip
 }
+
+output "security_group_id" {
+  description = "The id of the bastion security group"
+  value       = aws_security_group.this.id
+}
